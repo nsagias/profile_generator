80ce9f9892b26107d2b4096248c3558b4c96f63d
Nick <nsagias@gmail.com>
2021-09-26 11:03:21 -0400

    lint

--- survey.js
+++ survey.js
@@ -15,14 +15,14 @@ const rl = readline.createInterface({ input, output });
 let myAswers = [];
 console.log('Welcome to your profile generator');
 rl.question('What\'s your name? Nicknames are also acceptable :) -> ', (answer1) => {
-  myAswers.push(`My Name is -> ${answer1}`); 
+  myAswers.push(`My Name is -> ${answer1}`);
   rl.question('What\'s an activity you like doing? -> ', (answer2) => {
     myAswers.push(`What I love to do is -> ${answer2}`);
     rl.question(`What do you listen to while ${answer2} -> `, (answer3) => {
       myAswers.push(`While doing favourite activity of ${answer2} I listen to -> ${answer3}`);
       rl.question('Which meal is your favourite (eg: dinner, brunch, etc.) -> ', (answer4) => {
         myAswers.push(`My favourite meal of the day is -> ${answer4}`);
-        rl.question(`What\'s your favourite thing to eat for *${answer4}?* -> `, (answer5) => {
+        rl.question(`What's your favourite thing to eat for *${answer4}?* -> `, (answer5) => {
           myAswers.push(`For '${answer4}' I like to each ->  ${answer5}`);
           rl.question('Which sport is your absolute favourite? -> ', (answer6) => {
             myAswers.push(`My favourite sport is -> ${answer6}`);
